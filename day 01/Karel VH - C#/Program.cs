6777d3b9c0943905d5c45269ef6ea91bb774923c
Karel Vanden Houte <karel.vandenhoute@ae.be>
2021-12-01 08:09:58 +0100

    Linq

---
+++ day 01/Karel VH - C#/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+IEnumerable<int> increasedPartOne = Enumerable.Range(1, input.Length - 1)
+                  .Where(i => Convert.ToInt32(input[i]) > Convert.ToInt32(input[i - 1]));
+
+IEnumerable<int> increasedPartTwo = Enumerable.Range(3, input.Length - 3)
+                  .Where(i => input.Skip(i - 2).Take(3).Select(x => Convert.ToInt32(x)).Sum()
+                    > input.Skip(i - 3).Take(3).Select(x => Convert.ToInt32(x)).Sum());
+
+Console.WriteLine("Part 1: " + increasedPartOne.Count());
+Console.WriteLine("Part 2: " + increasedPartTwo.Count());

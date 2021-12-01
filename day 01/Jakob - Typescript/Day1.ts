e83a23c5b69666dfb24c8db98a97d2c60d9c047d
jakobdehertogh <jakob.dehertogh@ae.be>
2021-12-01 15:31:50 +0100

    part 2

--- day 01/Jakob - Typescript/Day1.ts
+++ day 01/Jakob - Typescript/Day1.ts
@@ -3,13 +3,21 @@ const text = fs.readFileSync("./input.txt", "utf-8");
 const textByLine = text.split("\r\n");
 const input = textByLine.map((str) => parseInt(str));
 
+const movingAmount = 3;
+
 let count = 0;
+let movingCount = 0;
 let i = 1;
 while (i < input.length) {
   if (input[i] > input[i - 1]) {
     count++;
   }
+  if (i > movingAmount - 1 && input[i] > input[i - movingAmount]) {
+    // moving amount increases if value i is greater than the value it replaces, i.e. the one x places ago.
+    movingCount++;
+  }
   i++;
 }
 
-console.log(count);
+console.log("count: " + count);
+console.log("movingCount: " + movingCount);

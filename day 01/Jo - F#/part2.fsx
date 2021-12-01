74f0c2bd1c6def0a33e46f4df8370b568ffe2781
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-12-01 07:00:32 +0100

    day 1 in f#, including some overengineer flexing

---
+++ day 01/Jo - F#/part2.fsx
@@ -0,0 +1,37 @@
+let input =
+    System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
+
+let example =
+    @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+
+let isIncrease (n1, n2) = n1 < n2
+
+let solve input =
+    input
+    |> Seq.map int
+    |> Seq.windowed 3
+    |> Seq.map Seq.sum
+    |> Seq.pairwise
+    |> Seq.filter isIncrease
+    |> Seq.length
+
+#r "nuget: Unquote"
+open Swensen.Unquote
+
+let run () =
+    printf "Testing..."
+    test <@ example.Split("\n") |> solve = 5 @>
+    printfn "...done!"
+
+run ()
+
+input |> solve

0c9d4cae317aa29cae515e0f277d730d0c937872
jakobdehertogh <jakob.dehertogh@ae.be>
2021-12-02 17:56:19 +0100

    Day 2 part 2 : big numbers

--- day 02/Jakob - Typescript/Day2.ts
+++ day 02/Jakob - Typescript/Day2.ts
@@ -9,22 +9,24 @@ const input: Command[] = textByLine.map((str) => new Command(str));
 
 let horpos: number = 0;
 let depth: number = 0;
+let aim: number = 0;
 
 for (let command of input) {
   switch (command.movement) {
     case Movement.DOWN: {
-      depth += command.amount;
+      aim += command.amount;
       break;
     }
     case Movement.UP: {
-      depth -= command.amount;
-      if (depth < 0) {
-        depth = 0;
-      }
+      aim -= command.amount;
       break;
     }
     case Movement.FORWARD: {
       horpos += command.amount;
+      depth += command.amount * aim;
+      if (depth < 0) {
+        depth = 0;
+      }
       break;
     }
   }
@@ -33,3 +35,4 @@ for (let command of input) {
 console.log("horizontal position: " + horpos);
 console.log("depth: " + depth);
 console.log("product: " + depth * horpos);
+console.log("aim: " + aim);

d04d5e473d523be9910ca4df056c99720d5a05f4
Martijn Millecamp <martijn.millecamp@ae.be>
2021-12-02 08:35:01 +0100

    solution day 2

---
+++ day 02/Martijn - Python/calculate_path_submarine.py
@@ -0,0 +1,21 @@
+import os
+print(os.getcwd())
+# instructions_parsed = [[instruction.split(' ')[0], int(instruction.split(' ')[1])] for instruction in open('day 02\Martijn - Python\input.txt').readlines()]
+instructions_parsed = [[instruction, int(nb)] for [instruction,nb] in [line.split() for line in open('day 02\Martijn - Python\input.txt').readlines()]] #slightly more clean solution of Toon
+
+horizontal_pos = 0
+depth = 0
+aim = 0
+for instruction in instructions_parsed:
+    command = instruction[0]
+    number = instruction[1]
+    if command == 'forward':
+        horizontal_pos += number
+        depth += (aim * number)
+    elif command == 'up':
+        aim -= number
+    elif command == 'down':
+        aim += number
+    else:
+        print('unknown command')
+print(horizontal_pos, depth, horizontal_pos * depth)

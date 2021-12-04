d7e751f4ab90f6fef944195d2d284f5609dd192c
Karel Vanden Houte <karel.vandenhoute@ae.be>
2021-12-04 10:05:51 +0100

    day04

---
+++ day 04/Karel VH - C#/Board.cs
@@ -0,0 +1,51 @@
+﻿namespace day04
+{
+    internal record Number
+    {
+        public string Val;
+
+        public Number(string val)
+        {
+            Val = val;
+        }
+
+        public bool Found = false;
+    }
+    internal class Board
+    {
+        public List<List<Number>> HorizontalNumbers { get; set; }
+        public List<List<Number>> VerticalNumbers { get; set; }
+
+        public Board(IEnumerable<string> boardNumbers)
+        {
+            HorizontalNumbers = boardNumbers.Select(x => x.Trim().Replace("  ", " ").Split(" ").Select(y => new Number(y)).ToList()).ToList();
+            VerticalNumbers = Enumerable.Range(0, boardNumbers.Count())
+                .Select(x => boardNumbers.Select(y => y.Trim().Replace("  ", " ").Split(" ")[x]).Select(y => new Number(y)).ToList()).ToList();
+        }
+
+
+        internal void Update(string nr)
+        {
+            HorizontalNumbers.ForEach(row => row.ForEach(n => { if (n.Val.Equals(nr)) n.Found = true; }));
+            VerticalNumbers.ForEach(row => row.ForEach(n => { if (n.Val.Equals(nr)) n.Found = true; }));
+        }
+
+        public List<string> NumbersLeft
+        {
+            get
+            {
+                //Only count horizontal since we copied all results to vertical aswell
+                return HorizontalNumbers.Select(x => x.Where(y => !y.Found)).SelectMany(r => r).Select(x => x.Val).ToList();
+            }
+        }
+
+        public bool IsComplete
+        {
+            get
+            {
+                return HorizontalNumbers.Any(x => x.Count(y => y.Found) == HorizontalNumbers.Count)
+                    || VerticalNumbers.Any(x => x.Count(y => y.Found) == VerticalNumbers.Count);
+            }
+        }
+    }
+}
---
+++ day 04/Karel VH - C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using day04;
+
+var input = File.ReadAllLines("input.txt");
+
+string[] gameCommands = input[0].Split(",");
+
+List<Board> boards = Enumerable.Range(0, input.Length / 6).Select(x => new Board(input.Skip(2 + 6 * x).Take(5))).ToList();
+
+int index = 0;
+Dictionary<Board, int> winOrder = new();
+while (index < gameCommands.Length && winOrder.Keys.Count < boards.Count)
+{
+    foreach (Board b in boards)
+    {
+        if (!b.IsComplete)
+            b.Update(gameCommands[index]);
+        else if (!winOrder.ContainsKey(b))
+            winOrder.Add(b, index);
+    }
+    index++;
+}
+Board firstBoard = winOrder.MaxBy(x => x.Value).Key;
+Board lastBoard = winOrder.MaxBy(x => x.Value).Key;
+Console.WriteLine(firstBoard.NumbersLeft.Select(x => int.Parse(x)).Sum() * int.Parse(gameCommands[winOrder[firstBoard]]));
+Console.WriteLine(lastBoard.NumbersLeft.Select(x => int.Parse(x)).Sum() * int.Parse(gameCommands[winOrder[lastBoard]]));

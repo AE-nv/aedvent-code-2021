4443e5916b52bcad38b85062bfb2efeb0035f0c2
Karel Vanden Houte <karel.vandenhoute@ae.be>
2021-12-05 13:48:41 +0100

    Corrected mistake + some refactoring

--- day 04/Karel VH - C#/Program.cs
+++ day 04/Karel VH - C#/Program.cs
@@ -1,25 +1,21 @@
-﻿using day04;
-
-var input = File.ReadAllLines("input.txt");
+﻿var input = File.ReadAllLines("input.txt");
 
 string[] gameCommands = input[0].Split(",");
 
-List<Board> boards = Enumerable.Range(0, input.Length / 6).Select(x => new Board(input.Skip(2 + 6 * x).Take(5))).ToList();
+List<Board> boards = Enumerable.Range(0, input.Length / 6).Select(x => new Board(input.Skip(2 + 6 * x).Take(5).Select(x => x.Trim().Replace("  ", " ")))).ToList();
 
-int index = 0;
+int index = -1;
 Dictionary<Board, int> winOrder = new();
-while (index < gameCommands.Length && winOrder.Keys.Count < boards.Count)
+while (++index < gameCommands.Length && winOrder.Keys.Count < boards.Count)
 {
-    foreach (Board b in boards)
+    boards.ForEach(b =>
     {
         if (!b.IsComplete)
             b.Update(gameCommands[index]);
         else if (!winOrder.ContainsKey(b))
             winOrder.Add(b, index);
-    }
-    index++;
+    });
 }
-Board firstBoard = winOrder.MaxBy(x => x.Value).Key;
+Board firstBoard = winOrder.MinBy(x => x.Value).Key;
 Board lastBoard = winOrder.MaxBy(x => x.Value).Key;
-Console.WriteLine(firstBoard.NumbersLeft.Select(x => int.Parse(x)).Sum() * int.Parse(gameCommands[winOrder[firstBoard]]));
-Console.WriteLine(lastBoard.NumbersLeft.Select(x => int.Parse(x)).Sum() * int.Parse(gameCommands[winOrder[lastBoard]]));
+Console.WriteLine((firstBoard.NumbersLeftSum * int.Parse(gameCommands[winOrder[firstBoard]]), lastBoard.NumbersLeftSum * int.Parse(gameCommands[winOrder[lastBoard]])));

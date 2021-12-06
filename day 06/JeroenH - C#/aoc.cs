a3edc3cc3063edbcc2c2d13e8c5cb2ca8556aaf3
jhgbrt <jeroen@haegebaert.com>
2021-12-06 07:13:27 +0100

    day 6

---
+++ day 06/JeroenH - C#/aoc.cs
@@ -0,0 +1,24 @@
+var input = File.ReadAllText("input.txt");
+var numbers = input.Split(',').Select(int.Parse).ToImmutableArray();
+var part1 = CountFish(80);
+var part2 = CountFish(256);
+Console.WriteLine((part1, part2));
+long CountFish(int iterations)
+{
+    var noffish = new long[9];
+    foreach (var n in numbers)
+        noffish[n]++;
+    for (int i = 0; i < iterations; i++)
+    {
+        var age6 = noffish[0];
+        for (int age = 1; age < 9; age++)
+        {
+            noffish[age - 1] = noffish[age];
+        }
+
+        noffish[6] += age6;
+        noffish[8] = age6;
+    }
+
+    return noffish.Sum();
+}

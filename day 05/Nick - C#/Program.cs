f69186101efbd3c17f8dedc1ddffbc953ba07bc8
Nick Verstraete <nick.verstraete@fluxys.com>
2021-12-06 09:28:21 +0100

    day 5 & 6

---
+++ day 05/Nick - C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines(@"input.txt").ToList();
+            Console.WriteLine(lines.Select(x => x.Split(" -> ")).SelectMany(x =>  GetPoints(x)).GroupBy(x => x).Count(x => Enumerable.Count(x) >= 2));            
+
+            List<Tuple<int, int>> GetPoints(string[] line)
+            {
+                var start = line.First().Split(",").Select(x => int.Parse(x));
+                var end = line.Last().Split(",").Select(x => int.Parse(x));
+                var pointList = new List<Tuple<int, int>>();              
+                foreach (var i in Enumerable.Range(0, Math.Max(Math.Abs((int)(start.First() - end.First())), Math.Abs((int)(start.Last() - end.Last()))) + 1)) 
+                {
+                    pointList.Add(new Tuple<int, int>(
+                         start.First() > end.First() ? end.First() + i : start.First() < end.First() ? end.First() - i : end.First(),
+                         start.Last() > end.Last() ? end.Last() + i : start.Last() < end.Last() ? end.Last() - i : end.Last()));
+                }               
+                return pointList;
+            }            
+        }
+    }
+}
---
+++ day 06/Nick - C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines(@"input.txt").First().Split(",").Select(x => int.Parse(x)).ToList();
+            var day = 0;
+            var dictInput = input.GroupBy(l => l).ToDictionary(grp => grp.Key, grp => (decimal)grp.Count());
+            while (day < 256) {
+                dictInput = dictInput.ToDictionary(grp => grp.Key - 1, grp => grp.Value);
+                if (dictInput.ContainsKey(-1)) 
+                {
+                    if (dictInput.ContainsKey(6)) dictInput[6] += dictInput[-1];
+                    else dictInput.Add(6, dictInput[-1]);
+                    dictInput.Add(8, dictInput[-1]);
+                    dictInput.Remove(-1);
+                }               
+                day++;
+            }
+            Console.WriteLine(dictInput.Sum(x => x.Value));
+        }
+    }
+}

3461ad9e85f0c2a85fe4d687045f562a1db6979d
Thibaut <thibaut.decremer@ae.be>
2021-12-15 07:09:14 +0100

    python ecosystem for the win

---
+++ day 15/Thibaut - python/day15.py
@@ -0,0 +1,31 @@
+import numpy as np
+from skimage.graph import route_through_array
+
+def p1(matrix):
+    start=[0,0]
+    end=[matrix.shape[0]-1,matrix.shape[1]-1]
+    path, score = route_through_array(matrix, start, end, fully_connected=False)
+
+    print("call")
+    scoreCheck=sum([matrix[node[0], node[1]] for node in path[1:]])
+    print("p1:",scoreCheck)
+
+if __name__ == '__main__':
+    with open("input.txt", 'r') as f:
+        data = f.readlines()
+
+    matrix = np.array([[int(c) for c in l.strip()] for l in data])
+    expandedMatrixShape = [matrix.shape[0]*5,matrix.shape[1]*5]
+    expandedMatrix = np.zeros(expandedMatrixShape)
+    expandedMatrix[0:100,0:100] = matrix
+    for r in range(0,5):
+        for c in range(0,5):
+            #print(r,c)
+            tmp = np.array(matrix)
+            for i in range(0,r+c):
+                tmp+=1
+                tmp = np.where(tmp > 9, 1, tmp)
+            #print(tmp)
+            expandedMatrix[r*100:(r+1)*100,c*100:(c+1)*100]= tmp
+    p1(expandedMatrix)
+

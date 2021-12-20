397c34271f14c6eb0aa4b6eb849d215687e17362
Karel Vanden Houte <karel.vandenhoute@ae.be>
2021-12-20 19:30:27 +0100

    c#

---
+++ bytes-booze/c#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+string programs = "abcdefghijklmnop";
+
+var input = File.ReadAllLines("input.txt")
+            .First().Split(",").Select(x =>
+            {
+                switch (x[0])
+                {
+                    case 's':
+                        Spin(int.Parse(x[1..].ToString()));
+                        break;
+                    case 'x':
+                        string[] split = x[1..].Split("/");
+                        SwapInt(int.Parse(split[0].ToString()), int.Parse(split[1].ToString()));
+                        break;
+                    case 'p':
+                        SwapChar(x[1], x[3]);
+                        break;
+                }
+                return x;
+            }).ToArray();
+
+void SwapChar(char v1, char v2)
+{
+    SwapInt(programs.IndexOf(v1), programs.IndexOf(v2));
+}
+
+
+void SwapInt(int v1, int v2)
+{
+    programs = new StringBuilder(programs) { [v1] = programs[v2], [v2] = programs[v1] }.ToString();
+}
+
+void Spin(int v)
+{
+    programs = programs.Substring(programs.Length - v) + programs.Substring(0, programs.Length - v);
+}
+
+Console.WriteLine(programs);
+
+//pnegmkdjhbfiolca
+
+//pnegmkdjhbfiolca

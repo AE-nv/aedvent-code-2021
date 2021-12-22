58909faf2d91a5cc642ec96ba5b0a75ee0b610bd
Karel Vanden Houte <karel.vandenhoute@ae.be>
2021-12-22 22:03:19 +0100

    day22

---
+++ day 22/Karel VH - C#/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+
+int xMin = 0;
+int yMin = 0;
+int zMin = 0;
+int xMax = 0;
+int yMax = 0;
+int zMax = 0;
+List<Instruction> instructions = File.ReadAllLines("input.txt").Select(x =>
+{
+    var k = new Regex(@"(.*?)\s.*?=(.*?)\.\.(.*?),.*?=(.*?)\.\.(.*?),.*?=(.*?)\.\.(.*)").Match(x).Groups.Values.ToList();
+    var action = k[1].Value.ToString();
+    var nrs = k.Skip(2).Select(x => int.Parse(x.Value)).ToList();
+    if (nrs[0] < xMin) xMin = nrs[0];
+    if (nrs[1] < xMax) xMax = nrs[1];
+    if (nrs[2] < yMin) yMin = nrs[2];
+    if (nrs[3] < yMax) yMax = nrs[3];
+    if (nrs[4] < zMin) zMin = nrs[4];
+    if (nrs[5] < zMax) zMax = nrs[5];
+    return new Instruction(action, nrs[0], nrs[1], nrs[2], nrs[3], nrs[4], nrs[5]);
+}).ToList();
+int cubeSize = 50;
+
+
+HashSet<(int, int, int)> cubes = new();
+
+int changed = 0;
+foreach (var inst in instructions.Where(x => x.X1 > -cubeSize && x.X2 < cubeSize && x.Y1 > -cubeSize && x.Y2 < cubeSize && x.Z1 > -cubeSize && x.Z2 < cubeSize))
+{
+    // Console.WriteLine(inst.Action);
+    for (int i = inst.X1; i <= inst.X2; i++)
+    {
+        for (int j = inst.Y1; j <= inst.Y2; j++)
+        {
+            for (int k = inst.Z1; k <= inst.Z2; k++)
+            {
+                if (inst.Action.Equals("on"))
+                {
+                    if (!cubes.Contains((i, j, k)))
+                    {
+                        //Console.WriteLine((i, j, k));
+                        changed++;
+                        cubes.Add((i, j, k));
+                    }
+                }
+                if (inst.Action.Equals("off"))
+                {
+                    if (cubes.Contains((i, j, k)))
+                    {
+                        //Console.WriteLine((i, j, k));
+                        changed++;
+                        cubes.Remove((i, j, k));
+
+                    }
+                }
+            }
+        }
+    }
+    Console.WriteLine(changed);
+    changed = 0;
+}
+
+Console.WriteLine(cubes.Count());
+
+
+record Instruction(string Action, int X1, int X2, int Y1, int Y2, int Z1, int Z2);
---
+++ day 22/Karel VH - C#/p2.cs
@@ -0,0 +1,175 @@
+﻿using System.Text.RegularExpressions;
+
+long xMin = 0;
+long yMin = 0;
+long zMin = 0;
+long xMax = 0;
+long yMax = 0;
+long zMax = 0;
+List<Cuboid> cuboids = File.ReadAllLines("input.txt").Select(x =>
+{
+    var k = new Regex(@"(.*?)\s.*?=(.*?)\.\.(.*?),.*?=(.*?)\.\.(.*?),.*?=(.*?)\.\.(.*)").Match(x).Groups.Values.ToList();
+    var action = k[1].Value.ToString();
+    var nrs = k.Skip(2).Select(x => long.Parse(x.Value)).ToList();
+    if (nrs[0] < xMin) xMin = nrs[0];
+    if (nrs[1] < xMax) xMax = nrs[1];
+    if (nrs[2] < yMin) yMin = nrs[2];
+    if (nrs[3] < yMax) yMax = nrs[3];
+    if (nrs[4] < zMin) zMin = nrs[4];
+    if (nrs[5] < zMax) zMax = nrs[5];
+    return new Cuboid() { On = action.Equals("on"), XStart = nrs[0], XEnd = nrs[1] + 1, YStart = nrs[2], YEnd = nrs[3] + 1, ZStart = nrs[4], ZEnd = nrs[5] + 1 };
+}).ToList();
+
+List<Cuboid> data = new();
+
+//Had to check scrape reddit for this
+foreach (Cuboid current in cuboids)
+{
+    List<Cuboid> newCuboids = new();
+    foreach (Cuboid cmp in data)
+    {
+        Cuboid compare = new(cmp);
+
+        if (current.XEnd > compare.XStart && current.XStart < compare.XEnd
+            && current.YEnd > compare.YStart && current.YStart < compare.YEnd
+            && current.ZEnd > compare.ZStart && current.ZStart < compare.ZEnd)
+        {
+            //Houston, we're dealing with an overlap
+
+            if (compare.XStart < current.XStart)
+            {
+                Cuboid newC = new Cuboid()
+                {
+                    On = compare.On,
+                    XStart = compare.XStart,
+                    XEnd = current.XStart,
+                    YStart = compare.YStart,
+                    YEnd = compare.YEnd,
+                    ZStart = compare.ZStart,
+                    ZEnd = compare.ZEnd
+                };
+                compare.XStart = current.XStart;
+                newCuboids.Add(newC);
+            }
+            if (compare.XEnd > current.XEnd)
+            {
+                Cuboid newC = new Cuboid()
+                {
+                    On = compare.On,
+                    XStart = current.XEnd,
+                    XEnd = compare.XEnd,
+                    YStart = compare.YStart,
+                    YEnd = compare.YEnd,
+                    ZStart = compare.ZStart,
+                    ZEnd = compare.ZEnd
+                };
+                compare.XEnd = current.XEnd;
+                newCuboids.Add(newC);
+            }
+            if (compare.YStart < current.YStart)
+            {
+                Cuboid newC = new Cuboid()
+                {
+                    On = compare.On,
+                    XStart = compare.XStart,
+                    XEnd = compare.XEnd,
+                    YStart = compare.YStart,
+                    YEnd = current.YStart,
+                    ZStart = compare.ZStart,
+                    ZEnd = compare.ZEnd
+                };
+                compare.YStart = current.YStart;
+                newCuboids.Add(newC);
+            }
+            if (compare.YEnd > current.YEnd)
+            {
+                Cuboid newC = new Cuboid()
+                {
+                    On = compare.On,
+                    XStart = compare.XStart,
+                    XEnd = compare.XEnd,
+                    YStart = current.YEnd,
+                    YEnd = compare.YEnd,
+                    ZStart = compare.ZStart,
+                    ZEnd = compare.ZEnd
+                };
+                compare.YEnd = current.YEnd;
+                newCuboids.Add(newC);
+            }
+            if (compare.ZStart < current.ZStart)
+            {
+                Cuboid newC = new Cuboid()
+                {
+                    On = compare.On,
+                    XStart = compare.XStart,
+                    XEnd = compare.XEnd,
+                    YStart = compare.YStart,
+                    YEnd = compare.YEnd,
+                    ZStart = compare.ZStart,
+                    ZEnd = current.ZStart,
+                };
+                compare.ZStart = current.ZStart;
+                newCuboids.Add(newC);
+            }
+            if (compare.ZEnd > current.ZEnd)
+            {
+                Cuboid newC = new Cuboid()
+                {
+                    On = compare.On,
+                    XStart = compare.XStart,
+                    XEnd = compare.XEnd,
+                    YStart = compare.YStart,
+                    YEnd = compare.YEnd,
+                    ZStart = current.ZEnd,
+                    ZEnd = compare.ZEnd
+                };
+                compare.ZEnd = current.ZEnd;
+                newCuboids.Add(newC);
+            }
+        }
+        else
+        {
+            newCuboids.Add(compare);
+        }
+    }
+    newCuboids.Add(current);
+    data = newCuboids;
+}
+
+long total = 0;
+foreach (var c in data)
+{
+    if (c.On)
+    {
+        total += (c.XEnd - c.XStart) * (c.YEnd - c.YStart) * (c.ZEnd - c.ZStart);
+    }
+}
+Console.WriteLine(total);
+
+class Cuboid
+{
+    public Cuboid() { }
+    public Cuboid(Cuboid cmp)
+    {
+        On = cmp.On;
+        XStart = cmp.XStart;
+        XEnd = cmp.XEnd;
+        YStart = cmp.YStart;
+        YEnd = cmp.YEnd;
+        ZStart = cmp.ZStart;
+        ZEnd = cmp.ZEnd;
+    }
+
+    public bool On { get; set; }
+    public long XStart { get; set; }
+    public long XEnd { get; set; }
+    public long YStart { get; set; }
+    public long YEnd { get; set; }
+    public long ZStart { get; set; }
+    public long ZEnd { get; set; }
+
+    public override string ToString()
+    {
+        return XStart + ", " + XEnd + ", " + YStart + ", " + YEnd + ", " + ZStart + ", " + ZEnd;
+    }
+}
